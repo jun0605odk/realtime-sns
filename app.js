80f0ccd2507634c9cd3c7532c71682b9f19940b0
jun0605odk <jun0605odk@gmail.com>
2020-07-04 18:43:22 +0900

    test4 commit

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
- res.statusCode = 200;
- res.setHeader('Content-Type', 'text/plain');
- res.end('Hello World!!!\n');
-}).listen(process.env.PORT || 3000, () => {
- console.log(`Server running.`);
-});
