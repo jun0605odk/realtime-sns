e8775d7b88ec7a585f6f295d8addc5f64a848a48
jun0605odk <jun0605odk@gmail.com>
2020-08-25 00:50:58 +0900

    heroku upload

---
+++ db/db.js
@@ -0,0 +1,42 @@
+
+// const pg = require('pg');
+// require('dotenv').config();
+
+// exports.pool = pg.Pool ({
+//   host: process.env.ENV_HOST,
+//   databese: process.env.ENV_DB,
+//   user: process.env.ENV_USER,
+//   port: 5432,
+//   password: process.env.ENV_PASSWORD,
+// });
+
+// const { Client } = require('pg')
+// const client = new Client({
+//     user: 'postgres',
+//     host: '192.168.2.103',
+//     database: 'realtime_sns_db',
+//     password: '9665JOst',
+//     port: 5432,
+// })
+// client.connect()
+// client.query('SELECT NOW()', (err, res) => {
+//     console.log(err, res)
+//     client.end()
+// })
+
+
+const { Client } = require('pg');
+require('dotenv').config();
+
+exports.client = new Client({
+    user: process.env.ENV_USER,
+    host: process.env.ENV_HOST,
+    databese: process.env.ENV_DATABASE,
+    port: process.env.ENV_PORT,
+    password: process.env.ENV_PASSWORD,
+});
+// client.connect()
+// client.query('SELECT NOW()', (err, res) => {
+//     console.log(err, res)
+//     client.end()
+// })
